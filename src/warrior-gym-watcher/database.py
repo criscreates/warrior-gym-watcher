bc5d248e1e454e7b5b65c0cbb76993076a304b6a
Cristian Moretto <cmoretto@uwaterloo.ca>
2026-03-18 19:23:47 -0400

    Change database infrastructure to use Supabase

--- src/warrior-gym-watcher/database.py
+++ src/warrior-gym-watcher/database.py
@@ -1,16 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from .models import Base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DB_URL = "sqlite:///./ticket_tracker.db"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, executemany_mode="values_only")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def init_db():
-    # create physical tables in the .db file
+    # create DB tables in the cloud Postgres database
     Base.metadata.create_all(bind=engine)
 
 
