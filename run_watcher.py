c4446d766cc1015205d072413afd7097dacedc79
Cristian Moretto <cmoretto@uwaterloo.ca>
2026-03-18 20:12:25 -0400

    Write basic web scraping script and verify its functionality

--- run_watcher.py
+++ run_watcher.py
@@ -1,3 +1,27 @@
 import sys
 import os
 from warrior_gym_watcher.database import SessionLocal, init_db
+from warrior_gym_watcher.services import FacilityOccupancyService
+from warrior_gym_watcher.crud import sync_scraped_data
+
+
+def run():
+    init_db()
+    service = FacilityOccupancyService()
+    raw_data: list[dict] = service.fetch_occupancy()
+
+    if not raw_data:
+        print("No data found, portal may be down!")
+        return
+
+    try:
+        with SessionLocal() as db:
+            sync_scraped_data(db, raw_data)
+            print("Data successfully fetched!")
+    except Exception as e:
+        print(f"Database sync failed: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    run()
--- src/warrior_gym_watcher/database.py
+++ src/warrior_gym_watcher/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, executemany_mode="values_only")
+engine = create_engine(DATABASE_URL, connect_args={"prepare_threshold": None})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
